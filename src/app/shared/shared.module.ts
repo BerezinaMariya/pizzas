7640c01d13c31e135ebc02b2a6b2117d1a65c31f
BerezinaMariya <BerezinaMariya.spb@yandex.ru>
2024-07-06 17:05:46 +0300

    add docker

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -13,8 +13,6 @@ import {MatMenuModule} from "@angular/material/menu";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 
-
-
 @NgModule({
   declarations: [
     ProductCardComponent,
